f94fb4151285ea14bf7fbc5f5050100fbf2bc4dd
Liam Adams <liam_adams@mckinsey.com>
2020-08-19 13:50:02 -0500

    top order working

--- hw1b/fncs.py
+++ hw1b/fncs.py
@@ -1,4 +1,10 @@
 import numpy as np
+import networkx as nx
+
+
+def get_parents(node):
+	child_edges = edges[np.where(edges[:,1] == node)]
+	return child_edges[:,0]
 
 # This function computed the topological ordering a graph. It
 # takes as an input an array of edges. It returns a dictionary
@@ -14,11 +20,27 @@ def compTopOrder(edges):
 	
 	# Extracting the number of Nodes
 	N = len(nodes)
-
+	marked_nodes = { i : False for i in nodes }
+	#children = set(edges[:,1])
+	ord_i = 0
 	# TODO: You will need to change this code
-	for node in nodes:
-		Ord[node] = node
-		
+	while ord_i < N - 1:
+		for node in nodes:
+			if marked_nodes[node]:
+				continue
+			child_edges = edges[np.where(edges[:,1] == node)]
+			if child_edges.size > 0:
+				parents = child_edges[:,0]
+				parent_not_marked = False
+				for parent in parents:
+					if not marked_nodes[parent]:
+						parent_not_marked = True
+						break
+				if parent_not_marked:
+					continue
+			marked_nodes[node] = True
+			Ord[node] = ord_i
+			ord_i += 1
 	return Ord
 
 # This function performs the check for topological ordering.
@@ -45,3 +67,17 @@ def checkTopOrder(edges):
             flag = False
             break
     return flag
+
+
+def test():
+	G = nx.DiGraph()
+	G.add_edges_from(
+		[('A', 'B'), ('A', 'C'), ('D', 'B'), ('E', 'C'), ('E', 'F'),
+		('B', 'H'), ('B', 'G'), ('B', 'F'), ('C', 'G')])
+	Ord = compTopOrder(G.edges)
+	print('Ordering = ',Ord)
+	H = nx.relabel_nodes(G, Ord)
+	checkTopOrder(H.edges)
+
+if __name__ == '__main__':
+	test()
